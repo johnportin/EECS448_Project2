a5b3f86d320be30d8450bf357cd2d53ac686a448
Mingyoung Jeng <mingyoungjeng@gmail.com>
2021-03-13 14:07:08 -0600

    cleanup

--- board.py
+++ board.py
@@ -8,9 +8,8 @@ from pygame import event
 pygame.init()
 
 info = pygame.display.Info()
-screen_width,screen_height = info.current_w,info.current_h
-WINDOWWIDTH = screen_width-100 # 1670
-WINDOWHEIGHT =  screen_height-100 #850
+WINDOWWIDTH = info.current_w-100 # 1670
+WINDOWHEIGHT =  info.current_h-100 #850
 boardSize = (WINDOWWIDTH/2) - 100 # 750
 
 #Quick helper function for getting board coordinates
