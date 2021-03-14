9e0fb265f63fbaa5bb3e019b926b725fa938e2db
johnportin <jportin13@gmail.com>
2021-03-14 18:35:01 -0500

    Clean up some code in auxx.py

--- auxx.py
+++ auxx.py
@@ -74,13 +74,10 @@ def createParagraph(text, fontSize, textcolor, bgcolor, paragraphSize):
 
 	return paragraphSurf, paragraphSize
 
-# I stole this function from (somewhere) GIVE CREDIT
-
 def createText(text, fontSize, textcolor, bgcolor):
     font = pygame.freetype.SysFont("Courier", fontSize, bold=True)
     surface, rect = font.render(text=text, fgcolor=textcolor, bgcolor=bgcolor)
     return surface
-    # return surface.convert_alpha()
 
 def quitGame():
     pygame.quit()
